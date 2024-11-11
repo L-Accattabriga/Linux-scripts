bdc2572617a4d780071aef5636a3a2580d195534
L-Accattabriga <L-Accattabriga@proton.me>
2024-11-11 09:04:11 +0000

    Update live-iso-setup.sh
    Added echo for each command

--- live-iso-setup.sh
+++ live-iso-setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-
+echo -e "\nsudo passwd user\n"
 sudo passwd user
 
+echo -e "\nsudo passwd\n"
 sudo passwd
 
+echo -e "\nsudo apt update\n"
 sudo apt update
 
+echo -e "\nsudo apt install acpi lm-sensors vim byobu\n"
 sudo apt install acpi lm-sensors vim byobu
 
 #----------
 
+echo -e "\nsudo sensors-detect\n"
 sudo sensors-detect
 
+echo -e "\nsudo sensors\n"
 sudo sensors
